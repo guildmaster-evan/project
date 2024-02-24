6a7037b590b6ec19f5ce650bf4f383d6bc8322f5
Evan Hernandez <161148324+guildmaster-evan@users.noreply.github.com>
2024-02-24 10:54:00 -0600

    corrected file names in about\img\

--- about.php
+++ about.php
@@ -10,8 +10,8 @@
     <div class="container">
         <div><img src="about\img\20230910_091808.jpg" alt="Coffee 1"></div>
         <div><img src="about\img\20230912_093007.jpg" alt="Coffee 2"></div>
-        <div><img src="about\img\20230915_070820.jpg" alt="Coffee 3"></div>
-        <div><img src="about\img\20230917_101237.jpg" alt="Coffee 4"></div>
+        <div><img src="about\img\20230915_070820.jpeg" alt="Coffee 3"></div>
+        <div><img src="about\img\20230917_101237.jpeg" alt="Coffee 4"></div>
     </div>
 
 </body>
