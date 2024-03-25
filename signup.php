472f27c3b0d5ab4f98fc61544ceccd3f882c6c38
Evan Hernandez <161148324+guildmaster-evan@users.noreply.github.com>
2024-03-24 21:33:07 -0500

    working signup page
    added hashing

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Signup Page</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 
@@ -12,10 +12,10 @@
     <?php include 'header.php'; ?>
 
     <div class="login-page-container">
-        <form action="includes\signup.inc.php" method="post" class="login-form">
+        <form action="includes/signup.inc.php" method="post" class="login-form">
             <h2 class="login-title">Sign up</h2>
 
-            <label for="username" class="login-label">Email or Phone:</label>
+            <label for="username" class="login-label">Username:</label>
             <input type="text" id="username" name="username" required class="login-input">
 
             <label for="password" class="login-label">Password:</label>
@@ -30,10 +30,10 @@
             <input type="hidden" name="redirect" value="login">
             <input type="hidden" name="message" value="Login successful!">
 
-            <button type="submit" class="login-button">Sign in</button>
+            <button type="submit" name=signup-submit class="login-button">Sign up</button>
         </form>
 
-        <p class="signup-link">Already have an account? <a href="login.php">Join now</a>.</p>
+        <p class="signup-link">Already have an account? <a href="login.php">Login Now</a>.</p>
     </div>
 
     <?php include 'footer.php'; ?>
